ddececee3c7e8eeb49192ec404c672e784c96bbf
Rosa Truneh <rosa.truneh@gmail.com>
2021-08-04 18:53:35 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
-
-
-
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Main from './Components/Main';
+import {animalData} from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-      <Main/>
+  // set modalOpen to false
+  // const [modalOpen, setModalOpen] = useState(false);
+  //switches this to the opposite
+  // let toggleModal = () => {
+  //   setModalOpen(prevModalOpen => !prevModalOpen) 
+  // };
 
-      {/* <header className="App-header">
-    //      <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-  //   </header>*/}
+  return (
+     <div className="App">
+       <Header/>
+       {/* passing into main so main can use them.  */}
+       <Main animalData={animalData}/>
+       <Footer/>
     </div>
   );
 }
