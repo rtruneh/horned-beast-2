1456c56412353c8fb204cb1fc2337abb236eb375
Rosa Truneh <rosa.truneh@gmail.com>
2021-08-04 18:34:56 -0500

    finished lab-01

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-    return <footer><p>Rosa Truneh</p></footer>;
+  return <footer><p>Rosa Truneh</p></footer>;
 }
 
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,21 +5,19 @@ import Black from '../img/hb2.jpg';
 import Gazelle from '../img/hb1.jpg';
 
 function Main() {
-    return (
-      <>
-        <HornedBeast 
-          title="Horned Animal" 
-          imgurl={Black} 
-          desc="black beauty"
-        />
-        <HornedBeast 
-          title="Gazelle" 
-          imgurl={Gazelle} 
-        />
-      </>
-      );
-    }
+  return (
+    <>
+      <HornedBeast 
+        title="Horned Animal" 
+        imgurl={Black} 
+        desc="black beauty"
+      />
+      <HornedBeast 
+        title="Gazelle" 
+        imgurl={Gazelle} 
+      />
+    </>
+  );
+}
 
 export default Main;
-
-//test//
