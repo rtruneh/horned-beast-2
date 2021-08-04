3b03f1d25494d7bab9dccea660f9db100ee7da8d
Rosa Truneh <rosa.truneh@gmail.com>
2021-08-04 18:47:12 -0500

    edited main.js function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,12 @@
 import React from 'react';
-
 import HornedBeast from './HornedBeast';
-// import Black from '../img/hb2.jpg';
-// import Gazelle from '../img/hb1.jpg';
-
-import {animalData} from '../data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 
 
-function Main() {
+function Main({animalData}) {
     return (
-      <div>
+      <>
         <Container>
           <Row>
             {animalData.map((data, key) => {
@@ -25,14 +20,13 @@ function Main() {
                       title={data.title}
                       description={data.description}
                       />
-                    
                   </div>
                 </Col>
               );
             })};
           </Row>
         </Container>
-      </div>
+      </>
         );
       }
 
